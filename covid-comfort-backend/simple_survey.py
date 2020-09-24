75b016827edb926a7ff53191c598674021ef7795
Kevin <kevinfserrano@gmail.com>
2020-09-24 09:48:46 -0700

    Displays some text from simple_survey.py

--- covid-comfort-backend/simple_survey.py
+++ covid-comfort-backend/simple_survey.py
@@ -1,14 +1,14 @@
-#!/usr/bin/python
-
+import time
+from flask import Flask
 import random
 import math
 import argparse
-from flask import Flask
 
 app = Flask(__name__)
 
 MAX_DIST = 10
 MAX_NODES = 20
+
 
 def generate_random_positions(num):
     positions = []
@@ -24,7 +24,9 @@ def generate_random_positions(num):
     return positions, distances
 
 def print_simulation(positions):
-    print ("\nSIMULATED ENVIRONMENT:\n")
+    result = "\nSIMULATED ENVIRONMENT:\n"
+    print (result)
+    result += '<br />'
     for y in range(-1*MAX_DIST, MAX_DIST):
         sim_line = ""
         for x in range(-1*MAX_DIST, MAX_DIST):
@@ -34,7 +36,9 @@ def print_simulation(positions):
             else:
                 sim_line += " X" if spot in positions else " ."
         print(sim_line)
+        result += sim_line + '\n'
     print ("\n")
+    return result + '\n'
 
 # Math functions
 def arithmetic_mean(listA):
@@ -50,19 +54,18 @@ def quadratic_mean(listA):
     squares = [value**2 for value in listA]
     return math.sqrt(float(sum(squares)) / float(len(listA)))
 
-@app.route('/covid-comfort')
-def main():
-    parser = argparse.ArgumentParser(description='Simulates positional situations of strangers walking around you in public.')
-    parser.add_argument( '--file', action = 'store', type = str, required = True, \
-        help = 'File to record responses.')
-    args = parser.parse_args()
+@app.route('/cc')
+def get_current_time():
+    result = ""
 
     while True:
+    # while False:
         # Generate positions
         positions, distances = generate_random_positions(random.randint(1,MAX_NODES))
         # Print positions
-        print_simulation(positions)
+        result = print_simulation(positions)
         
+        # risk_score = 4 #todo: figure out input
         risk_score = input("Rate perceived risk from 1 (feels very safe) to (feels very unsafe):\n")
         while True:
             try:
@@ -70,22 +73,27 @@ def main():
                 assert risk_score > 0 and risk_score <= 5
                 break
             except:
+                # risk_score = 4 #todo: figure out 
                 risk_score = input("Enter a valid rating.\n")
 
         # Build feature list + Label
         # Closest Distance, Arithmetic Mean, Geometric Mean, Quadratic Mean, Label(Risk), Distances, Positions
-        try:
-            f = open(args.file)
-            f.close()
-        except:
-            print("Making new file...")
-            with open(args.file, 'w+') as f:
-                f.write("NUM\tMIN_DIST\tA_MEAN\tG_MEAN\tQ_MEAN\tRISK\tDISTANCES\tPOSITIONS\n")
-        with open(args.file, 'a') as f:
-            fields = [len(distances), min(distances), arithmetic_mean(distances), \
-                geometric_mean(distances), quadratic_mean(distances), risk_score, distances, positions]
-            fields = [str(field) for field in fields]
-            f.write("\t".join(fields) + "\n")
+        #TODO: Figure out this file stuff
+        # try:
+        #     f = open(args.file)
+        #     f.close()
+        # except:
+        #     # print("Making new file...") #todo: figure out prints
+        #     with open(args.file, 'w+') as f:
+        #         f.write("NUM\tMIN_DIST\tA_MEAN\tG_MEAN\tQ_MEAN\tRISK\tDISTANCES\tPOSITIONS\n")
+        # with open(args.file, 'a') as f:
+        #     fields = [len(distances), min(distances), arithmetic_mean(distances), \
+        #         geometric_mean(distances), quadratic_mean(distances), risk_score, distances, positions]
+        #     fields = [str(field) for field in fields]
+        #     f.write("\t".join(fields) + "\n")
+
+        # return "test"
+        return result #NOTE: This will not loop
  
 if __name__ == "__main__":
     main()
--- covid-comfort-frontend/src/App.js
+++ covid-comfort-frontend/src/App.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(1);
+
+  useEffect(() => {
+    fetch('/cc').then(res => res.text()).then(data => {
+      setCurrentTime(data);
+    });
+  }, []);
+
+  // useEffect(() => {
+  //   fetch('/').then(res => res.text()).then(data => {
+  //     setCurrentTime(data);
+  //   });
+  // });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{currentTime}</p>
       </header>
     </div>
   );
